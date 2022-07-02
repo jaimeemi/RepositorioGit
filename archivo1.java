c14fa586d5e75a28f8fc4aff1cd948404804b355
Emilio Del Pin <emiliodelpin@gmail.com>
2022-07-02 15:28:17 -0300

    Commit Incial V1.0

---
+++ archivo1.java
@@ -0,0 +1,5 @@
+public class archivo1{
+    public static void main(String[] args ){
+        System.out.println("Hola Emilio, Este es mi primer branch");
+    }
+}
---
+++ archivo2.java
@@ -0,0 +1,11 @@
+public class archivo2 {
+    private int Edad;
+    private String Nombre;
+    private String Apellido;
+    private int id;
+    private double Salario;
+
+    public archivo2(){
+
+    }
+}
